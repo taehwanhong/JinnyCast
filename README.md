0de14c7a0d7196923b9f48e931327761606c9e59
davidhong <taehwan.hong@gmail.com>
2017-03-28 15:45:59 +0900

    JS 분리

--- README.md
+++ README.md
@@ -5,6 +5,6 @@
 
 ### Project Title
 
-### 
+### Project Overview
 
-<iframe src="https://docs.google.com/presentation/d/1pYOOLWBS6Ywwv8EPSftxA9uMSa1-VQpGaeahmXqDNRE/embed?start=false&loop=false&delayms=3000" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+[https://docs.google.com/presentation/d/1pYOOLWBS6Ywwv8EPSftxA9uMSa1-VQpGaeahmXqDNRE/embed?start=false&loop=false&delayms=3000]
--- search.js
+++ search.js
@@ -1,18 +1,54 @@
-// After the API loads, call a function to enable the search box.
-function handleAPILoaded() {
-    $('#search-button').attr('disabled', false);
-}
+/// Search API
+function fnGetList(sGetToken) {
+    var $getval = $("#search_box").val();
+    if ($getval == "") {
+        alert == ("뭐임마");
+        $("#search_box").focus();
+        return;
+    }
 
-// Search for a specified string.
-function search() {
-    var q = $('#query').val();
-    var request = gapi.client.youtube.search.list({
-        q: q,
-        part: 'snippet'
-    });
 
-    request.execute(function(response) {
-        var str = JSON.stringify(response.result);
-        $('#search-container').html('<pre>' + str + '</pre>');
+    // clear //
+    $("#get_view").empty();
+    $("#nav_view").empty();
+    $("#player").empty();
+
+
+    // query section //
+    var sTargetUrl = "https://www.googleapis.com/youtube/v3/search?part=snippet&order=relevance" +
+        "&q=" + encodeURIComponent($getval) + "&key=AIzaSyDjBfDWFgQa6bdeLc1PAM8EoDAFB_CGYig";
+    if (sGetToken) {
+        sTargetUrl += "&pageToken=" + sGetToken;
+    }
+
+    $.ajax({
+        type: "POST",
+        url: sTargetUrl,
+        dataType: "jsonp",
+        success: function(jdata) {
+            // console.log(jdata.items[0].id.videoId);
+            firstID = jdata.items[0].id.videoId;
+            console.log(firstID);
+            onYouTubeIframeAPIReady();
+            $(jdata.items).each(function(i) {
+                // console.log(this.snippet.channelId);
+                $("#get_view").append("<p class='box'><a href='https://youtu.be/'" + this.id.videoId + ">" + this.snippet.title + "</a></p>"); //list 보여주기
+            }).promise().done(function() {
+                    if (jdata.prevPageToken) {
+                        $("#nav_view").append("<a href='javascript:fnGetList(\"" + jdata.prevPageToken + "\");'><이전페이지></a>");
+                    }
+                    if (jdata.nextPageToken) {
+                        $("#nav_view").append("<a href='javascript:fnGetList(\"" + jdata.nextPageToken + "\");'><다음페이지></a>");
+                    }
+                }
+
+            );
+            return jdata;
+        },
+        error: function(xhr, textStatus) {
+            console.log(xhr.responseText);
+            alert("error");
+            return;
+        }
     });
 }
