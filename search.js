99f8b565eef689032dd5b7b709f4e981d6f71c1d
davidhong <taehwan.hong@gmail.com>
2017-03-29 16:03:36 +0900

    dev1.3 -> 1.3.1 checkout)

--- search.js
+++ search.js
@@ -2,14 +2,13 @@ var nameSpace = {
 
 };
 
-var firstID = '';
-var $getval = '';
-var getvideoId = [];
+nameSpace.$getval = '';
+nameSpace.getvideoId = [];
 
 
 var fnGetList = function(sGetToken) {
-    $getval = $("#search_box").val();
-    if ($getval == "") {
+    nameSpace.$getval = $("#search_box").val();
+    if (nameSpace.$getval == "") {
         alert == ("검색어 입력바랍니다.");
         $("#search_box").focus();
         return;
@@ -18,12 +17,12 @@ var fnGetList = function(sGetToken) {
     //Dom setting
     $("#get_view").empty();
     $("#nav_view").empty();
-    $("#player_view").empty();
+    $("#videoPlayer").empty();
 
     // query section //
     // 15개씩
-    var sTargetUrl = "https://www.googleapis.com/youtube/v3/search?part=snippet&order=relevance&maxResults=15&type=video" + "&q=" + encodeURIComponent($getval) + "&key=AIzaSyDjBfDWFgQa6bdeLc1PAM8EoDAFB_CGYig";
-    getvideoId = [];
+    var sTargetUrl = "https://www.googleapis.com/youtube/v3/search?part=snippet&order=relevance&maxResults=15&type=video" + "&q=" + encodeURIComponent(nameSpace.$getval) + "&key=AIzaSyDjBfDWFgQa6bdeLc1PAM8EoDAFB_CGYig";
+    nameSpace.getvideoId = [];
     if (sGetToken) {
         sTargetUrl += "&pageToken=" + sGetToken;
     }
@@ -36,17 +35,17 @@ var fnGetList = function(sGetToken) {
 
             $(jdata.items).each(function(i) {
                 $("#get_view").append("<p class='box'><a href=https://youtu.be/ " + this.id.videoId + ">" + this.snippet.title + "</a></p>"); //list 보여주기
-                getvideoId.push(jdata.items[i].id.videoId);
-                console.log(getvideoId);
+                nameSpace.getvideoId.push(jdata.items[i].id.videoId);
+                console.log(nameSpace.getvideoId);
             }).promise().done(function() {
-                if (jdata.prevPageToken) {
-                    $("#nav_view").append("<a href='javascript:fnGetList(\"" + jdata.prevPageToken + "\");'><이전페이지></a>");
-                }
+                // if (jdata.prevPageToken) {
+                //     $("#nav_view").append("<a href='javascript:fnGetList(\"" + jdata.prevPageToken + "\");'><이전페이지></a>");
+                // }
 
-                if (jdata.nextPageToken) {
-                    $("#nav_view").append("<a href='javascript:fnGetList(\"" + jdata.nextPageToken + "\");'><다음페이지></a>");
-                }
-                $("#player_view").append("<iframe src=https://www.youtube.com/embed/" + getvideoId[1] + "?rel=0&enablejsapi=1 frameborder=0 allowfullscreen></iframe>");
+                // if (jdata.nextPageToken) {
+                //     $("#nav_view").append("<a href='javascript:fnGetList(\"" + jdata.nextPageToken + "\");'><다음페이지></a>");
+                // }
+                $("#videoPlayer").append("<iframe src=https://www.youtube.com/embed/" + nameSpace.getvideoId[1] + "?rel=0&enablejsapi=1 frameborder=0 allowfullscreen></iframe>");
             });
         },
         error: function(xhr, textStatus) {
