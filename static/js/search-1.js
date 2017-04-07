3face2b5bf36a3225316242bb2a34a290f887b30
davidhong <taehwan.hong@gmail.com>
2017-04-07 12:48:17 +0900

    ajax post init

--- static/js/search-1.js
+++ static/js/search-1.js
@@ -1,20 +1,20 @@
 /* 2017. 03. 
-*/
+ */
 
 
 /* ======= Responsive Web ======= */
 const hPX = {
     header: 50,
-    audioPlayer : 80,
-    inputBox : 45
+    audioPlayer: 80,
+    inputBox: 45
 }
 
-const resizeMainHeight = function(){
-  util.$("#main").style.height = window.innerHeight - hPX.header - hPX.audioPlayer +'px';
-  util.$(".searchList").style.height = window.innerHeight - hPX.header - hPX.audioPlayer - hPX.inputBox + 'px';
+const resizeMainHeight = function() {
+    util.$("#main").style.height = window.innerHeight - hPX.header - hPX.audioPlayer + 'px';
+    util.$(".searchList").style.height = window.innerHeight - hPX.header - hPX.audioPlayer - hPX.inputBox + 'px';
 }
 
-window.addEventListener('resize',function(){
+window.addEventListener('resize', function() {
     resizeMainHeight();
 });
 
@@ -26,7 +26,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 /* ======= Utility ======= */
 var util = {
-    runAjax : function(url, listener, reqFunc){
+    runAjax: function(url, listener, reqFunc) {
         let oReq = new XMLHttpRequest();
         oReq.addEventListener(listener, reqFunc);
         oReq.open("GET", url);
@@ -35,47 +35,47 @@ var util = {
     $: function(selector) {
         return document.querySelector(selector);
     },
-    $$: function(selector){
+    $$: function(selector) {
         return document.querySelectorAll(selector);
     },
-    timeAgo: function(selector){
+    timeAgo: function(selector) {
 
-    var templates = {
-        prefix: "",
-        suffix: " ago",
-        seconds: "less than a minute",
-        minute: "about a minute",
-        minutes: "%d minutes",
-        hour: "about an hour",
-        hours: "about %d hours",
-        day: "a day",
-        days: "%d days",
-        month: "about a month",
-        months: "%d months",
-        year: "about a year",
-        years: "%d years"
-    };
-    var template = function(t, n) {
-        return templates[t] && templates[t].replace(/%d/i, Math.abs(Math.round(n)));
-    };
+        var templates = {
+            prefix: "",
+            suffix: " ago",
+            seconds: "less than a minute",
+            minute: "about a minute",
+            minutes: "%d minutes",
+            hour: "about an hour",
+            hours: "about %d hours",
+            day: "a day",
+            days: "%d days",
+            month: "about a month",
+            months: "%d months",
+            year: "about a year",
+            years: "%d years"
+        };
+        var template = function(t, n) {
+            return templates[t] && templates[t].replace(/%d/i, Math.abs(Math.round(n)));
+        };
 
-    var timer = function(time) {
-        if (!time)
-            return;
-        time = time.replace(/\.\d+/, ""); // remove milliseconds
-        time = time.replace(/-/, "/").replace(/-/, "/");
-        time = time.replace(/T/, " ").replace(/Z/, " UTC");
-        time = time.replace(/([\+\-]\d\d)\:?(\d\d)/, " $1$2"); // -04:00 -> -0400
-        time = new Date(time * 1000 || time);
+        var timer = function(time) {
+            if (!time)
+                return;
+            time = time.replace(/\.\d+/, ""); // remove milliseconds
+            time = time.replace(/-/, "/").replace(/-/, "/");
+            time = time.replace(/T/, " ").replace(/Z/, " UTC");
+            time = time.replace(/([\+\-]\d\d)\:?(\d\d)/, " $1$2"); // -04:00 -> -0400
+            time = new Date(time * 1000 || time);
 
-        var now = new Date();
-        var seconds = ((now.getTime() - time) * .001) >> 0;
-        var minutes = seconds / 60;
-        var hours = minutes / 60;
-        var days = hours / 24;
-        var years = days / 365;
+            var now = new Date();
+            var seconds = ((now.getTime() - time) * .001) >> 0;
+            var minutes = seconds / 60;
+            var hours = minutes / 60;
+            var days = hours / 24;
+            var years = days / 365;
 
-        return templates.prefix + (
+            return templates.prefix + (
                 seconds < 45 && template('seconds', seconds) ||
                 seconds < 90 && template('minute', 1) ||
                 minutes < 45 && template('minutes', minutes) ||
@@ -87,23 +87,23 @@ var util = {
                 days < 365 && template('months', days / 30) ||
                 years < 1.5 && template('year', 1) ||
                 template('years', years)
-                ) + templates.suffix;
-    };
+            ) + templates.suffix;
+        };
 
-    var elements = document.querySelectorAll('.videoTimeAgo');
-    for (var i in elements) {
-        var $this = elements[i];
-        if (typeof $this === 'object') {
-            $this.innerHTML = timer($this.getAttribute('title') || $this.getAttribute('datetime'));
+        var elements = document.querySelectorAll('.videoTimeAgo');
+        for (var i in elements) {
+            var $this = elements[i];
+            if (typeof $this === 'object') {
+                $this.innerHTML = timer($this.getAttribute('title') || $this.getAttribute('datetime'));
+            }
         }
-    }
     }
 }
 
 
 /* ======= Youtube API Setting ======= */
-const setTargetURL = function(keyword, sGetToken){
-    
+const setTargetURL = function(keyword, sGetToken) {
+
     const baseURL = 'https://www.googleapis.com/youtube/v3/search?part=snippet&';
     var setting = {
         order: 'viewCount',
@@ -112,13 +112,13 @@ const setTargetURL = function(keyword, sGetToken){
         q: keyword,
         key: 'AIzaSyDjBfDWFgQa6bdeLc1PAM8EoDAFB_CGYig'
     }
- 
+
     let sTargetURL = Object.keys(setting).map(function(k) {
         return encodeURIComponent(k) + "=" + encodeURIComponent(setting[k]);
     }).join('&')
-    
+
     sTargetURL = baseURL + sTargetURL;
-    
+
     if (sGetToken) {
         sTargetURL += "&pageToken=" + sGetToken;
     }
@@ -128,7 +128,7 @@ const setTargetURL = function(keyword, sGetToken){
 
 /* ======= Model ======= */
 const youtubeAPISearchResult = {
-    init: function(){
+    init: function() {
         this.allVideos = json; //처음 로딩될떄 모든 데이터를 가져옵니다.
     },
     selectedVideoID: null, //선택한 값
@@ -136,58 +136,58 @@ const youtubeAPISearchResult = {
 };
 
 const videoSearchListController = {
-    init: function(){
+    init: function() {
         searchListView.init();
     },
-    getAllVideos: function(){
+    getAllVideos: function() {
         return youtubeAPISearchResult.allVideos.items;
     },
-    getNextPageToken: function(){
+    getNextPageToken: function() {
         return youtubeAPISearchResult.nextPageTokenNumer;
     },
-    setNextPageToken: function(){
+    setNextPageToken: function() {
         youtubeAPISearchResult.nextPageTokenNumer = youtubeAPISearchResult.allVideos.nextPageToken;
     },
-    getSelectedVideoID: function(){
+    getSelectedVideoID: function() {
         return youtubeAPISearchResult.selectedVideoID
     },
-    setSelectedVideo: function(id){
+    setSelectedVideo: function(id) {
         id = youtubeAPISearchResult.selectedVideoID
     }
 }
 
 const searchListView = {
-   init: function(){
-       this.content = util.$(".searchList");
-       this.template = util.$("#searchVideo").innerHTML;
-       this.render();
-       this.events();
-    
-   },
-   render: function(){
-       videos = videoSearchListController.getAllVideos();
-       let sHTML = '';
-       for (let i=0; i < videos.length; i++) {
-           let videoImageUrl =  videos[i].snippet.thumbnails.default.url;
-           let videoTitle =  videos[i].snippet.title;
-           let publishedAt = videos[i].snippet.publishedAt;
-           let videoId = videos[i].id.videoId
-           sDom = this.template.replace("{videoImage}", videoImageUrl)
-           .replace("{videoTitle}", videoTitle)
-           .replace("{time}", publishedAt)
-           .replace("{videoPublishedAt}", publishedAt)
-           .replace("{videoId}", videoId);
+    init: function() {
+        this.content = util.$(".searchList");
+        this.template = util.$("#searchVideo").innerHTML;
+        this.render();
+        this.events();
+
+    },
+    render: function() {
+        videos = videoSearchListController.getAllVideos();
+        let sHTML = '';
+        for (let i = 0; i < videos.length; i++) {
+            let videoImageUrl = videos[i].snippet.thumbnails.default.url;
+            let videoTitle = videos[i].snippet.title;
+            let publishedAt = videos[i].snippet.publishedAt;
+            let videoId = videos[i].id.videoId
+            sDom = this.template.replace("{videoImage}", videoImageUrl)
+                .replace("{videoTitle}", videoTitle)
+                .replace("{time}", publishedAt)
+                .replace("{videoPublishedAt}", publishedAt)
+                .replace("{videoId}", videoId);
             sHTML = sHTML + sDom;
         }
         this.content.insertAdjacentHTML('beforeend', sHTML);
     },
-    
-    callSearchAPI: function(){
+
+    callSearchAPI: function() {
         util.$(".goSearch").addEventListener('click', function(event) {
             util.$(".searchList").innerHTML = "";
             this.searchKeyword = util.$("#search_box").value;
             sUrl = setTargetURL(this.searchKeyword);
-            util.runAjax(sUrl, "load", function(){
+            util.runAjax(sUrl, "load", function() {
                 json = JSON.parse(this.responseText);
                 youtubeAPISearchResult.init();
                 videoSearchListController.init();
@@ -198,33 +198,33 @@ const searchListView = {
         });
     },
 
-    moreSearchResult: function(){
+    moreSearchResult: function() {
         this.searchKeyword = util.$("#search_box").value;
-        util.$(".searchList").addEventListener("scroll", function(){
-            if(this.scrollHeight - this.scrollTop === this.clientHeight) {
+        util.$(".searchList").addEventListener("scroll",  function() {            
+            if (this.scrollHeight  -  this.scrollTop  ===  this.clientHeight)  {
                 nextPageTok = videoSearchListController.getNextPageToken();
-                sUrl = setTargetURL(this.searchKeyword, nextPageTok);
-                util.runAjax(sUrl, "load",function(){
+                sUrl = setTargetURL(this.searchKeyword, nextPageTok);               
+                util.runAjax(sUrl,  "load", function() {
                     json = JSON.parse(this.responseText);
-                    
+
                     youtubeAPISearchResult.init();
-                    
+
                     videoSearchListController.init();
                     util.timeAgo();
                     videoSearchListController.setNextPageToken();
-                });
-            }
-        });  
+                });            
+            }        
+        });
     },
-    events: function(){
+    events: function() {
         util.$(".searchList").addEventListener('click', function(evt) {
             target = evt.target;
-            if (target.tagName === 'I'){
+            if (target.tagName === 'I') {
                 target = target.parentNode;
                 (console.log(target));
             }
-            if (target.tagName !== "BUTTON"){ 
-                target = util.$(".videoInfo"); 
+            if (target.tagName !== "BUTTON") {
+                target = util.$(".videoInfo");
                 util.$(".previewModal").dataset.id = '';
                 util.$(".previewModal").classList.remove("hide");
                 sDom = util.$("#previewVideo").innerHTML;
@@ -239,28 +239,25 @@ const searchListView = {
             // elem =  elem.closest(".videoInfo");  
             // (console.log(elem));      
             // if (!elem) return;
-            
+
         });
-        
+
     },
-    hidePreview: function(){
+    hidePreview: function() {
         util.$(".close_btn").addEventListener('click', function(evt) {
-            let button =  evt.target.closest("button");
+            let button = evt.target.closest("button");
             util.$(".previewModal").classList.add("hide");
             util.$(".searchList").classList.remove("modal-open");
         });
     },
-    
+
 
 }
 
-//  runAjax : function(url, listener, reqFunc){
-//         let oReq = new XMLHttpRequest();
-//         oReq.addEventListener(listener, reqFunc);
-//         oReq.open("GET", url);
-//         oReq.send();
-//     },
-    
+var postAlbum = function() {
+
+}
+
 
 
 // buttonLike.addEventListener("click", function(evt){
