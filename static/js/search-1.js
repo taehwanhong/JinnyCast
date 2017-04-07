578fda4f66de14a11281c4d6498c7126fecb9f21
sujinleeme <sujinlee.me@gmail.com>
2017-04-07 10:59:05 +0900

    [dev1.4] Add timeAgo function

--- static/js/search-1.js
+++ static/js/search-1.js
@@ -37,8 +37,69 @@ var util = {
     },
     $$: function(selector){
         return document.querySelectorAll(selector);
+    },
+    timeAgo: function(selector){
+
+    var templates = {
+        prefix: "",
+        suffix: " ago",
+        seconds: "less than a minute",
+        minute: "about a minute",
+        minutes: "%d minutes",
+        hour: "about an hour",
+        hours: "about %d hours",
+        day: "a day",
+        days: "%d days",
+        month: "about a month",
+        months: "%d months",
+        year: "about a year",
+        years: "%d years"
+    };
+    var template = function(t, n) {
+        return templates[t] && templates[t].replace(/%d/i, Math.abs(Math.round(n)));
+    };
+
+    var timer = function(time) {
+        if (!time)
+            return;
+        time = time.replace(/\.\d+/, ""); // remove milliseconds
+        time = time.replace(/-/, "/").replace(/-/, "/");
+        time = time.replace(/T/, " ").replace(/Z/, " UTC");
+        time = time.replace(/([\+\-]\d\d)\:?(\d\d)/, " $1$2"); // -04:00 -> -0400
+        time = new Date(time * 1000 || time);
+
+        var now = new Date();
+        var seconds = ((now.getTime() - time) * .001) >> 0;
+        var minutes = seconds / 60;
+        var hours = minutes / 60;
+        var days = hours / 24;
+        var years = days / 365;
+
+        return templates.prefix + (
+                seconds < 45 && template('seconds', seconds) ||
+                seconds < 90 && template('minute', 1) ||
+                minutes < 45 && template('minutes', minutes) ||
+                minutes < 90 && template('hour', 1) ||
+                hours < 24 && template('hours', hours) ||
+                hours < 42 && template('day', 1) ||
+                days < 30 && template('days', days) ||
+                days < 45 && template('month', 1) ||
+                days < 365 && template('months', days / 30) ||
+                years < 1.5 && template('year', 1) ||
+                template('years', years)
+                ) + templates.suffix;
+    };
+
+    var elements = document.querySelectorAll('.videoTimeAgo');
+    for (var i in elements) {
+        var $this = elements[i];
+        if (typeof $this === 'object') {
+            $this.innerHTML = timer($this.getAttribute('title') || $this.getAttribute('datetime'));
+        }
+    }
     }
 }
+
 
 /* ======= Youtube API Setting ======= */
 const setTargetURL = function(keyword, sGetToken){
@@ -113,7 +174,8 @@ const searchListView = {
            let videoId = videos[i].id.videoId
            sDom = this.template.replace("{videoImage}", videoImageUrl)
            .replace("{videoTitle}", videoTitle)
-           .replace("{videoViews}", publishedAt)
+           .replace("{time}", publishedAt)
+           .replace("{videoPublishedAt}", publishedAt)
            .replace("{videoId}", videoId);
             sHTML = sHTML + sDom;
         }
@@ -131,6 +193,7 @@ const searchListView = {
                 videoSearchListController.init();
                 videoSearchListController.setNextPageToken();
                 searchListView.moreResult();
+                util.timeAgo();
             });
         });
     },
@@ -143,8 +206,11 @@ const searchListView = {
                 sUrl = setTargetURL(this.searchKeyword, nextPageTok);
                 util.runAjax(sUrl, "load",function(){
                     json = JSON.parse(this.responseText);
+                    
                     youtubeAPISearchResult.init();
+                    
                     videoSearchListController.init();
+                    util.timeAgo();
                     videoSearchListController.setNextPageToken();
                 });
             }
